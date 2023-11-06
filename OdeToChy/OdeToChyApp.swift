f750fb1e988923aa4d92a51f3108f4d6363f388f
Taylor Carr <tayloracarr94@gmail.com>
2023-11-06 02:53:15 -0800

    Initial Commit

---
+++ OdeToChy/OdeToChyApp.swift
@@ -0,0 +1,32 @@
+//
+//  OdeToChyApp.swift
+//  OdeToChy
+//
+//  Created by Taylor Carr on 11/6/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct OdeToChyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
